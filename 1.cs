2587eb3e81a63aecd8260bf0af5351b99bcb8e63
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2026-09-23 20:54:11 +0200

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,21 @@
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello worlsdfsdfsdf!");
+            string nev = "Lóczy";
+            int kor = 23;
+            double atlag = 4.3;
+            bool aktiv = true;
+            Console.WriteLine("Név: " + nev + "\n" + "kor: " + kor+ "\natlag: " + atlag+ "\naktiv: " + aktiv);
+            Console.WriteLine($"név: {nev}");
+
+
+
+        }
+    }
+}
